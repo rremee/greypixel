fe8ff139714452c39a82b1e3f6ff45ada3e3c28c
Anton Valkovskyi <u1remited@gmail.com>
2026-07-21 13:11:33 +0300

    feat: add constants for navigation links

---
+++ src/constants/index.jsx
@@ -0,0 +1,7 @@
+export const navLinks = [
+	{ name: 'Home', href: '/', image: '/images/nav-home.webp' },
+	{ name: 'About', href: '/about', image: '/images/nav-about.webp' },
+	{ name: 'Work', href: '/works', image: '/images/nav-works.webp' },
+	{ name: 'Services', href: '/services', image: '/images/nav-services.webp' },
+	{ name: 'Contact', href: '/contact', image: '/images/nav-contact.webp' },
+];
